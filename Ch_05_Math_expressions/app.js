411a377d3e1c1f1260866aed643cb182a281f8ca
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-08-26 16:14:21 +0500

    added chapter 05

---
+++ Ch_05_Math_expressions/app.js
@@ -0,0 +1,32 @@
+console.log("running");
+
+//Chapter 05: Math expressions:- Familiar Operators
+
+// Wherever you can use a number, you can use a math expression. For example, you're
+// familiar with this kind of statement.
+
+var add = 5 + 5;
+console.log(add); // 10
+
+var subtract = 4 - 3;
+console.log(subtract); // 1
+
+var multiply = 4 * 5;
+console.log(multiply); //20
+
+var divide = 3 / 5;
+console.log(divide);  // 0.6
+
+var result = divide + add ;
+console.log(result); // 10.6
+
+// show the result in an alert box 
+// alert(result);
+
+//The arithmetic operators I've been using, +, -, *, and /, are undoubtedly familiar to you.
+// This one may not be:
+//  "%" a modulus operator in JavaScript
+//  ' % ' represent a reminder
+var whatsLeftOver = 10 % 2;
+console.log(whatsLeftOver);  // 0
+
